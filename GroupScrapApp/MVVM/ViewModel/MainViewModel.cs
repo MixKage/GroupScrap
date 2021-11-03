05a7aa1c07a22b7a7ea7e4587892db544d892b25
MixKage <shishov265@gmail.com>
2021-11-03 14:29:41 +0300

    Добавьте файлы проекта.

---
+++ GroupScrapApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using GroupScrapApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GroupScrapApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand SettingsViewCommand { get; set; }
+
+        public RelayCommand CloseCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public SettingsViewModel SettingsVM { get; set; }
+
+        public Action CloseAction { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            SettingsVM = new SettingsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+
+            CloseCommand = new RelayCommand(o =>
+            {
+                CloseAction();
+            });
+        }
+    }
+}
---
+++ GroupScrapApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupScrapApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MVVM.ViewModel.MainViewModel vm = new MVVM.ViewModel.MainViewModel();
+            this.DataContext = vm;
+            if (vm.CloseAction == null)
+                vm.CloseAction = new Action(this.Close);
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+    }
+}
