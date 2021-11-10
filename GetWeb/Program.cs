7ba7c05efde8fdc155fdc931ab9835979f6c9e8c
PowerLay <1854pro@gmail.com>
2021-11-10 18:18:13 +0300

    GetWeb: clean up

--- GetWeb/Program.cs
+++ GetWeb/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
+using System.IO;
+using CommandLine;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
-using System.IO;
-using System.Threading;
-using CommandLine;
 
 namespace GetWeb
 {
-    class Options
+    internal class Options
     {
         [Option('t', "timeout", Default = 10, HelpText = "Maximum waiting time for a response from the webinar.")]
         public double Timeout { get; set; }
@@ -21,19 +20,19 @@ namespace GetWeb
     }
 
 
-    class Program
+    internal class Program
     {
-        static Options CurrentOptions;
-        static int Main(string[] args)
+        private static Options CurrentOptions;
+
+        private static int Main(string[] args)
         {
             Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed<Options>(o =>
-                   { CurrentOptions = o; });
+                .WithParsed(o => { CurrentOptions = o; });
 
             var options = new ChromeOptions();
-            options.AddArgument("mute-audio");//Запуск без звука
+            options.AddArgument("mute-audio"); //Запуск без звука
 
-            string res = "";
+            var res = "";
 
             IWebDriver driver = new ChromeDriver(options);
             try
@@ -43,18 +42,20 @@ namespace GetWeb
 
                 do
                 {
-                    string pageSource = driver.PageSource;
+                    var pageSource = driver.PageSource;
                     if (pageSource.Contains("Подключиться"))
                     {
                         res = PrepairDirect(driver);
                         break;
                     }
-                    else if (pageSource.Contains("Посмотреть запись"))
+
+                    if (pageSource.Contains("Посмотреть запись"))
                     {
                         res = PrepairRecord(driver);
                         break;
                     }
-                    else if (pageSource.Contains("зарегистрироваться"))
+
+                    if (pageSource.Contains("зарегистрироваться"))
                     {
                         res = PrepairStream(driver);
                         break;
@@ -75,35 +76,42 @@ namespace GetWeb
             return 0;
         }
 
-        static void SetSliderPercentage(IWebDriver driver, string sliderHandleXpath, string sliderTrackXpath, int percentage)
+        private static void SetSliderPercentage(IWebDriver driver, string sliderHandleXpath, string sliderTrackXpath,
+            int percentage)
         {
             var sliderHandle = driver.FindElement(By.XPath(sliderHandleXpath));
             var sliderTrack = driver.FindElement(By.XPath(sliderTrackXpath));
             var width = int.Parse(sliderHandle.GetCssValue("Width").Replace("px", ""));
             var dx = (int)(percentage / 100.0 * width);
             new Actions(driver)
-                        .MoveToElement(sliderHandle, dx, 0)
-                        .Click()
-                        .Build()
-                        .Perform();
+                .MoveToElement(sliderHandle, dx, 0)
+                .Click()
+                .Build()
+                .Perform();
         }
-        static string PrepairRecord(IWebDriver driver)
+
+        private static string PrepairRecord(IWebDriver driver)
         {
             driver.FindElement(By.LinkText("ПОСМОТРЕТЬ ЗАПИСЬ")).Click(); // Войти в конференциею
             driver.FindElement(By.ClassName("autoplay-video-allow-btn")).Click();
 
-            SetSliderPercentage(driver, "/html/body/div[1]/div/div/div[2]/div/div", "/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]", 90);
+            SetSliderPercentage(driver, "/html/body/div[1]/div/div/div[2]/div/div",
+                "/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]", 90);
 
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[1]/main/div[1]/div[2]/button[3]")).Click(); //click участники
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[1]/main/div[1]/div[2]/button[3]"))
+                .Click(); //click участники
 
-            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            var js = (IJavaScriptExecutor)driver;
             js.ExecuteScript("document.body.style.zoom='5%'");
 
-            while (!driver.PageSource.Contains("Row.P_PARTICIPANTS.15")) { } //Костыль переделать
+            while (!driver.PageSource.Contains("Row.P_PARTICIPANTS.15"))
+            {
+            } //Костыль переделать
 
             return driver.PageSource;
         }
-        static string PrepairStream(IWebDriver driver)
+
+        private static string PrepairStream(IWebDriver driver)
         {
             driver.FindElement(By.LinkText("ЗАРЕГИСТРИРОВАТЬСЯ")).Click(); // Войти в конференциею
             driver.FindElement(By.Id("email")).SendKeys("1854pro2@gmail.com\tВроде\tЧеловек\t\t\t\t\n"); // Ввод данных
@@ -118,23 +126,30 @@ namespace GetWeb
 
             driver.FindElement(By.Id("name")).SendKeys("\t\n"); // Подтверждение данных
 
-            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/main/div[1]/div[2]/button[4]")).Click(); //click участники
+            var js = (IJavaScriptExecutor)driver;
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/main/div[1]/div[2]/button[4]"))
+                .Click(); //click участники
             js.ExecuteScript("document.body.style.zoom='5%'");
 
-            while (!driver.PageSource.Contains("Вроде")) { } //Костыль переделать
+            while (!driver.PageSource.Contains("Вроде"))
+            {
+            } //Костыль переделать
 
             return driver.PageSource;
         }
-        static string PrepairDirect(IWebDriver driver)
+
+        private static string PrepairDirect(IWebDriver driver)
         {
             driver.FindElement(By.Id("name")).SendKeys("\t\n"); // Подтверждение данных
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/main/div[1]/div[2]/button[4]")).Click(); //click участники
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/main/div[1]/div[2]/button[4]"))
+                .Click(); //click участники
 
-            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            var js = (IJavaScriptExecutor)driver;
             js.ExecuteScript("document.body.style.zoom='5%'");
 
-            while (!driver.PageSource.Contains("Вроде")) { } //Костыль переделать
+            while (!driver.PageSource.Contains("Вроде"))
+            {
+            } //Костыль переделать
 
             return driver.PageSource;
         }
